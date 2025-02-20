2ef08dce016179b552a670a810de0c1444275f56
changyunyeong <sally0109277@naver.com>
2025-02-20 22:50:33 +0900

    🎉  New Project

---
+++ malhaedo/src/main/java/com/backend/malhaedo/MalhaedoApplication.java
@@ -0,0 +1,13 @@
+package com.backend.malhaedo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalhaedoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MalhaedoApplication.class, args);
+	}
+
+}
---
+++ malhaedo/src/test/java/com/backend/malhaedo/MalhaedoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.malhaedo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalhaedoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
