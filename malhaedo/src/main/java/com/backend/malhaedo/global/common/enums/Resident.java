c3c5123724da58cd2ab2476a40d39f8af238632e
changyunyeong <sally0109277@naver.com>
2025-02-24 16:36:57 +0900

    ✨  feat: 주민 enums 추가 #17

---
+++ malhaedo/src/main/java/com/backend/malhaedo/global/common/enums/Resident.java
@@ -0,0 +1,16 @@
+package com.backend.malhaedo.global.common.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Resident {
+
+    BAEBDURI("뱁뚜리"),
+    DARAMI("다람이"),
+    PENGLE("펭글이"),
+    GOMDOONGI("곰둥이");
+
+    private final String sender;
+}
