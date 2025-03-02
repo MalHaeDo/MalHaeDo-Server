5fa8d3dab9ef6e908fbdf31a04c7edfd6cd9e70b
changyunyeong <sally0109277@naver.com>
2025-03-02 15:59:50 +0900

    ✨  feat: 임시 접근 허용 #25

--- malhaedo/src/main/java/com/backend/malhaedo/global/config/CorsConfig.java
+++ malhaedo/src/main/java/com/backend/malhaedo/global/config/CorsConfig.java
@@ -16,21 +16,24 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:8080"));
+//        config.setAllowedOrigins(List.of("http://localhost:8080"));
+        config.setAllowedOriginPatterns(List.of("*")); // TODO: 임시로 모든 도메인 허용
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
 
         // 쿠키 관련 설정
         config.setAllowCredentials(true);
 
-        config.setAllowedHeaders(List.of(
-                "Authorization",
-                "Content-Type",
-                "X-Requested-With",
-                "Accept",           // 쿠키 처리를 위해 추가 (RefreshToken 용)
-                "Origin",           // CORS 요청에 필요
-                "Access-Control-Request-Method",
-                "Access-Control-Request-Headers"
-        ));
+//        config.setAllowedHeaders(List.of(
+//                "Authorization",
+//                "Content-Type",
+//                "X-Requested-With",
+//                "Accept",           // 쿠키 처리를 위해 추가 (RefreshToken 용)
+//                "Origin",           // CORS 요청에 필요
+//                "Access-Control-Request-Method",
+//                "Access-Control-Request-Headers"
+//        ));
+
+        config.setAllowedHeaders(List.of("*")); // TODO: 임시로 모든 헤더 허용
 
         config.setExposedHeaders(List.of(
                 "Authorization",
