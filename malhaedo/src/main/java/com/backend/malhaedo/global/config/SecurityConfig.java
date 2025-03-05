740b4547700ea4efd5016aac0d78d8b28490358d
changyunyeong <sally0109277@naver.com>
2025-03-05 19:27:07 +0900

    ✨  feat: auth file 다운로드 #31

--- malhaedo/src/main/java/com/backend/malhaedo/global/config/SecurityConfig.java
+++ malhaedo/src/main/java/com/backend/malhaedo/global/config/SecurityConfig.java
@@ -45,7 +45,8 @@ public class SecurityConfig {
             "/v3/api-docs/**",
             "/api/v0/member/signup",
             "/api/v0/member/signup/guest",
-            "/api/v0/prompt/reply"
+            "/api/v0/prompt/reply",
+            "/.well-known/pki-validation/**"
     };
 
     @Bean
