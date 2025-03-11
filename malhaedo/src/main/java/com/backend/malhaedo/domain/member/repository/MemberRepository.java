993158b35d8635d1ec7ee98b7d14c39df1f0187b
changyunyeong <sally0109277@naver.com>
2025-03-11 19:52:31 +0900

    ✨  feat: 중복 에러 추가

--- malhaedo/src/main/java/com/backend/malhaedo/domain/member/repository/MemberRepository.java
+++ malhaedo/src/main/java/com/backend/malhaedo/domain/member/repository/MemberRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByProviderAndProviderId(String provider, String providerId);
+    boolean existsByNickName(String nickName);
+    boolean existsByIslandName(String islandName);
 }
